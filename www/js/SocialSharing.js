33a44fa7a229d406648db9ceae4ca16de24abe51
surajsinghnegi <surajsinghnegi009@gmail.com>
2018-07-24 02:10:48 +0530

    third

--- www/js/SocialSharing.js
+++ www/js/SocialSharing.js
@@ -26,11 +26,11 @@ function twitterShare(){
   };
 
  function otherShare(){
-   window.plugins.socialsharing.share('https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya', null, null, null);
+   window.plugins.socialsharing.share('350+ best stories like Betaal Pachchisi, Akbar-Birbal, Tenaliram, Inspirational,  Panchtantra, funny, Love and Lok-Kathayen all in one App. \n\nhttps://play.google.com/store/apps/details?id=com.devsun.hindikahaniya', null, null, null);
   };
 
  function storyShare(){
    var data = $('.text-flow').text();
-   window.plugins.socialsharing.share('Download app \n https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya \n\n'+data, null, null, null);
+   window.plugins.socialsharing.share('Download app for more hindi stories \n https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya \n\n'+data, null, null, null);
   };
 
