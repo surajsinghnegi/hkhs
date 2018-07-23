33fb967b706bb651309874c18c617a5db1683918
surajsinghnegi <surajsinghnegi009@gmail.com>
2018-07-24 02:13:04 +0530

    fourth

--- www/js/SocialSharing.js
+++ www/js/SocialSharing.js
@@ -29,7 +29,7 @@ function twitterShare(){
    window.plugins.socialsharing.share('350+ best stories like Betaal Pachchisi, Akbar-Birbal, Tenaliram, Inspirational,  Panchtantra, funny, Love and Lok-Kathayen all in one App. \n\nhttps://play.google.com/store/apps/details?id=com.devsun.hindikahaniya', null, null, null);
   };
 
- function storyShare(){
+ function storyShare(){ 
    var data = $('.text-flow').text();
    window.plugins.socialsharing.share('Download app for more hindi stories \n https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya \n\n'+data, null, null, null);
   };
