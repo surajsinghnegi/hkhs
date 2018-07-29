88e74630e7b40a0ac8a6aa3b51a434cc66080d33
surajsinghnegi <surajsinghnegi009@gmail.com>
2018-07-29 15:55:33 +0530

    4rth

--- platforms/android/app/src/main/assets/www/js/SocialSharing.js
+++ platforms/android/app/src/main/assets/www/js/SocialSharing.js
@@ -26,11 +26,11 @@ function twitterShare(){
   };
 
  function otherShare(){
-   window.plugins.socialsharing.share('https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya', null, null, null);
+ 	window.plugins.socialsharing.share('इस बेहतरीन ऐप के ज़रिए बच्चें अलग-अलग विषयवस्तु की कई मज़ेदार कहानियों का लुत्फ उठा सकते है। इस रोमांच के साथ- साथ ही वो रूबरू होंगे आदर्श मूल्यों से जो उनके जीवन में नैतिकता के मोती पिरोने में मददगार होंगे।. \nhttps://play.google.com/store/apps/details?id=com.devsun.hindikahaniya', null, null, null);
   };
 
- function storyShare(){
+ function storyShare(){ 
    var data = $('.text-flow').text();
-   window.plugins.socialsharing.share('Download app \n https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya \n'+data, null, null, null);
+   window.plugins.socialsharing.share('Download app for more hindi stories \n https://play.google.com/store/apps/details?id=com.devsun.hindikahaniya \n\n'+data, null, null, null);
   };
 
--- platforms/android/app/src/main/assets/www/js/contact.js
+++ platforms/android/app/src/main/assets/www/js/contact.js
@@ -1,3 +1,8 @@
+var i = 0;
 $(window).on('beforeunload', function(){
-   if(AdMob) AdMob.showInterstitial();
+	if(i%3===0)
+	{
+   		if(window.AdMob) AdMob.showInterstitial();
+	}
+	i++;
 });
